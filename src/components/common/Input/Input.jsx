b7a1268da0ee380e945f84518a320fa4da99753d
Minho <mhgeum07@gmail.com>
2026-01-13 11:59:53 +0900

    [FEAT] Input 공통 컴포넌트 및 스타일 구현

---
+++ src/components/common/Input/Input.jsx
@@ -0,0 +1,51 @@
+import { useId } from "react";
+import { Wrapper, Label, InputContainer, IconWrapper, Message, StyledInput } from "./styles";
+
+export const Input = ({
+  label,
+  type = "text",
+  errorMessage,
+  variant = "primary",
+  size = "md",
+  fullWidth = true,
+  startIcon,
+  endIcon,
+  className,
+  inputRef,
+  ...props
+}) => {
+  
+  const uniqueId = useId();
+  const inputId = props.id || uniqueId;
+
+  return (
+    <Wrapper $fullWidth={fullWidth} className={className}>
+      {label && <Label htmlFor={inputId}>{label}</Label>}
+
+      <InputContainer
+        $variant={variant}
+        $size={size}
+        $hasError={!!errorMessage}
+        $disabled={props.disabled}
+        onClick={() => {
+          if (props.disabled) return;
+          inputRef?.current?.focus();
+        }}
+      >
+        {startIcon && <IconWrapper>{startIcon}</IconWrapper>}
+
+        <StyledInput 
+          ref={inputRef} 
+          id={inputId} 
+          type={type} 
+          disabled={props.disabled} 
+          {...props} 
+        />
+
+        {endIcon && <IconWrapper>{endIcon}</IconWrapper>}
+      </InputContainer>
+
+      {errorMessage && <Message $isError={true}>{errorMessage}</Message>}
+    </Wrapper>
+  );
+};
