fb6cda36a0f6dd17818883567b9e5aa204ed577b
Minho <mhgeum07@gmail.com>
2026-01-17 17:26:21 +0900

    [FEAT] 비밀번호 재설정 라우팅 경로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Post from './pages/Post';
 import AuthPage from './pages/AuthPage';
 
 import './App.css';
+import PasswordResetVerifyPage from './components/auth/accountrecovery/PasswordResetVerifyPage';
+import PasswordResetFormPage from './components/auth/accountrecovery/PasswordResetFormPage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       {/* path="/"에 대한 element={}를 회원가입,로그인 부분으로 바꿔주세요  */}
       <Route path="/" element={<AuthPage />} />  
       <Route path="/post" element={<Post />} />
+      <Route path="/reset-password" element={<PasswordResetVerifyPage />} />
+      <Route path="/reset-password/update" element={<PasswordResetFormPage />} />
     </Routes>
   )
 }
