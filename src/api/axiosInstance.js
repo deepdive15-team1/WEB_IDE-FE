2f17f9989bd83a7e258a3f4365f828062dd654ca
san-sae <choi.yoo.lim.12@gmail.com>
2026-01-18 16:30:55 +0900

    [FEAT] 요청시 액세스 토큰이 헤더에 자동 포함하는 로직

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,5 +2,24 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL, // 추후 .env 설정
-  timeout: 10000
+  timeout: 10000,
+  headers: {
+    "Content-type": "application/json"
+  },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = "임시적 토큰"; // 추후 엑세스 토큰 상태관리 올린 다음 수정
+
+    if(token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+
+);
