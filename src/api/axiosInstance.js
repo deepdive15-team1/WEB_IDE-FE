f25eae2ba6f05380f6d1c0fa50608b9c4c9372b4
Minho <mhgeum07@gmail.com>
2026-01-19 02:56:39 +0900

    [FEAT] App.jsx 앱 진입 시 토큰 검증 및 Axios 자동 로그아웃 구현

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../stores/useAuthStore";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL // 추후 .env 설정
 
@@ -28,8 +29,9 @@ axiosInstance.interceptors.response.use(
   async (err) => {
     const originalRequest = err.config;
 
+    //401 에러이고 아직 재시도하지 않은 요청일 때
     if (err.response && err.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+      originalRequest._retry = true; // 무한 루프 방지
 
       try {
         const refreshToken = localStorage.getItem("refreshToken");
@@ -38,6 +40,7 @@ axiosInstance.interceptors.response.use(
           throw new Error("refreshToken이 없습니다.");
         }
 
+        //토큰 재발급 요청
         const { data } = await axios.post(`${BASE_URL}/auth/token`, {
           refreshToken: refreshToken,
         });
@@ -45,14 +48,14 @@ axiosInstance.interceptors.response.use(
         const newAccessToken = data.accessToken;
         localStorage.setItem("accessToken", newAccessToken);
 
+        // 실패했던 요청의 헤더를 새 토큰으로 교체 후 재요청
         originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
         return axiosInstance(originalRequest);
 
       } catch (refreshError) {
         console.error("토큰 재발급 실패:", refreshError);
 
-        localStorage.removeItem("accessToken");
-        localStorage.removeItem("refreshToken");
+        useAuthStore.getState().Logout();
 
         alert("로그인이 만료되었습니다. 다시 로그인해주세요.");
         window.location.href = "/";
