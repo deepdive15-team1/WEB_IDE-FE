d6b2874787ce4d20b2efe3fe897c3bee809b336a
san-sae <choi.yoo.lim.12@gmail.com>
2026-01-18 12:11:29 +0900

    [FIX] 기존 Post 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import Post from './pages/Post';
 import AuthPage from './pages/AuthPage';
 import PostCreate from './pages/PostCreate';
 import PostDetail from './pages/PostDetail';
+// import PostList from './pages/PostList';
 
 import './App.css';
 import PasswordResetVerifyPage from './components/auth/accountrecovery/PasswordResetVerifyPage';
@@ -12,11 +12,11 @@ import MyPage from './pages/MyPage';
 function App() {
   return (
     <Routes>
-      {/* path="/"에 대한 element={}를 회원가입,로그인 부분으로 바꿔주세요  */}
       <Route path="/" element={<AuthPage />} />  
-      <Route path="/post" element={<Post />} />
       <Route path="/post-create" element={<PostCreate />} />
       <Route path="/post-detail" element={<PostDetail />} />
+      {/* <Route path="/post-detail/:postId" element={<PostDetail />} /> */}
+      {/* <Route path="/post-list" element={<PostList />} /> */}
       <Route path="/reset-password" element={<PasswordResetVerifyPage />} />
       <Route path="/reset-password/update" element={<PasswordResetFormPage />} />
       <Route path="/mypage" element={<MyPage />} />
